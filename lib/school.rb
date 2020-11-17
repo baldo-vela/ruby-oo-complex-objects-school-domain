d0b35166992d136602f5cb258fa692498b347779
baldo-vela <baldo.vela+github@gmail.com>
2020-11-17 08:26:18 -0600

    commit tree 6c42f2c462ee3ad07fe42cc45616571631d81d07

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
 # code here!
+class School
+    attr_reader :name, :roster
+    
+    def initialize(school_name)
+        @school_name = school_name
+        @roster = {}
+    end
+    
+    def roster
+        @roster
+    end
+
+    def add_student(name, grade)
+        @student_name = name
+        @grade = grade
+        if @roster.include?(grade) == false
+            @roster[grade]=[]
+        end
+        @roster[grade] << name
+    end
+
+    def grade(grade)
+        #retreive students from a grade
+        @roster[grade]
+    end
+
+    def sort
+        @roster.each do |grade, name|
+            @roster[grade] = name.sort
+        end
+    end
+
+end
